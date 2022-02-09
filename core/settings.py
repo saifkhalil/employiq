866fc6f4bbe2152bf37a0504ff76d156e286529e
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2022-02-09 10:22:10 +0300

    update setting add STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -150,7 +150,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
