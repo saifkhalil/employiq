56936d52f0b77732c029a180286bb0ed9fe8b16f
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-09-17 00:42:41 +0300

    update gitignore

--- core/settings.py
+++ core/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import environ
 import os
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -21,6 +21,7 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
