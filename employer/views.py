af464cec1c0fa4ea81dfa3c0b4bdcf209dfcfe47
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-09-20 22:16:16 +0300

    check remaining jobs

--- employer/views.py
+++ employer/views.py
@@ -134,7 +134,7 @@ def pre_job_create(request):
                                  _(f"You don't have remaining Jobs balance for your current subscription {employer_subscription.plan}"))
             return redirect(reverse('home'))
         else:
-            return redirect(reverse_lazy('JobCreateView'))
+            return redirect(reverse_lazy('job_create'))
     else:
         messages.add_message(request, messages.ERROR,
                              _("You are not subscribed to us, please subscribe to post a job"))
