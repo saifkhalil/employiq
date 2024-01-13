f0d1d5e3504e63b93896d9fa7c40febaf805ca02
ski <saif.ibrahim@qi.iq>
2024-01-13 13:32:20 +0300

    active STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -221,7 +221,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
